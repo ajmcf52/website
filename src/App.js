c89bae587329558b0446370a25844d0ce6984c28
ajmcf52 <andrew.mcfarlane52@gmail.com>
2022-10-13 17:32:39 -0700

    changes noted in dd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./App.css";
-import SiteHeader from "./Components/SiteHeader";
-import Profile from "./Components/Profile";
-import DataRef from "./Components/DataRef";
-import InfoCard from "./Components/InfoCard";
+import HomePage from "./Components/HomePage";
 import { Provider } from "react-redux";
 
 import store from "./store";
@@ -12,12 +9,8 @@ function App() {
   return (
     <div className="topLevel">
       <Provider store={store}>
-        <DataRef />
         <div className="App">
-          <div className="background"></div>
-          <SiteHeader />
-          <Profile />
-          <InfoCard />
+          <HomePage />
         </div>
       </Provider>
     </div>
