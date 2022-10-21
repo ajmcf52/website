cf280bffe7ea961b53c2d952efb2b5b1aea9cd14
ajmcf52 <andrew.mcfarlane52@gmail.com>
2022-10-21 16:31:34 -0700

    changes noted in dd

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -7,6 +7,41 @@ const HomePage = ({ classes }) => {
   return (
     <div className="homePage">
       <HomePageHeader />
+      <hr className="hrule" />
+      <div className="box">
+        <p className="aboutHeader">About Me</p>
+        <p className="aboutText">
+          <span className="textChunk">
+            Self-proclaimed nerd jock. Loosely speaking, AJ's central mission in
+            life is to help as many people as possible and to have a good time
+            doing it. A student of stoicism, AJ is in pursuit of excellence in
+            technology and sport, namely software engineering and bodybuilding.
+            Had he been born around the time of Jesus, AJ would have gotten
+            along very well with the paleo-Christians.
+          </span>
+          <br />
+          <br />
+          <span className="textChunk">
+            Music-wise, the Viking Snow Yeti's palate is vast — from classical
+            and heavy metal, to drum & bass and rap. It mustn't be forgotten,
+            however, that Andrew's true loyalty lies with classic rock: the
+            sweet, golden nectar of the sixties and seventies. The gift that
+            just keeps giving. Supertramp, Doobie Brothers, Fleetwood Mac, Dire
+            Straits; how can you go wrong?
+          </span>
+          <br />
+          <br />
+          <span className="textChunk">
+            Historically and still predominantly an introvert, AJ can be
+            extroverted too
+          </span>
+          <br />
+          <br />
+          Dreamchaser. I make an effort of keeping most of these things to
+          myself (until there is merit to share them). Such things are best
+          revealed in small chunks over time, I've found.
+        </p>
+      </div>
     </div>
   );
 };
--- src/Components/HomePageHeader.js
+++ src/Components/HomePageHeader.js
@@ -1,8 +1,7 @@
 import React from "react";
-import PropTypes from "prop-types";
 import "./css/HomePageHeader.css";
 
-const HomePageHeader = ({ classes }) => {
+const HomePageHeader = () => {
   return (
     <div className="topLevelBox">
       <img
@@ -23,41 +22,48 @@ const HomePageHeader = ({ classes }) => {
         <p className="blurb">
           <span className="topBlurbChunk">
             My big interests are programming, exercise (i.e., weightlifting,
-            hiking, running, etc), and reading non-fiction with respect to
-            self-improvement, international relations, psychology, and mythology
-            among others. programming, creative writing, non-fiction literature,
-            hanging with friends when time-affordable, film, and nature make up
-            the bulk of my interests. The first four take up most of my time
-            these days.
+            hiking, running, etc), non-fiction books, writing, international
+            relations, and mythology. With writing, I enjoy creating mostly
+            prose and poetry. When time and responsibility afford it, I enjoy
+            hanging with friends, watching films, and getting outside.
           </span>{" "}
           <br /> <br />
           <span className="textChunk">
-            I have recently started a blog that I have been kicking myself to
-            start for the past couple of years. Feel free to check it out
-            (here)[]. Growing up, I played (nearly) every sport under the sun.
-            Football stuck with me the longest. I finished my 15-year career at
-            Hobart College as a defensive lineman.
+            If you are interested in checking out some of my software projects,{" "}
+            <b>
+              feel free to give them a look <a href="#top">here</a>.
+            </b>
           </span>
           <br />
           <br />
-          <span className="bottomHeaderChunk">
-            When I'm not working, you can typically find me at the gym or
-            Starbucks.
+          <span className="textChunk">
+            For the past two years, I have been kicking myself to start a blog.
+            I finally did it. You might find it interesting. Take a look{" "}
+            <a href="#top">here</a>.
+          </span>
+          <br />
+          <br />
+          <span className="textChunk">
+            I've kept note of the books I've read over the past couple years, as
+            well as books I'm currently reading and plan on reading. If you
+            care, you can see a list of these <a href="#top">here</a>.
           </span>
         </p>
-        <p>
-          "The man who says he can, and the man who says he can not.. Are both
-          correct"
-          <br />- Confucius
-        </p>
-        <p>
-          "It is better to be a warrior in a garden, than a gardener in war."
-          <br />- Unknown
-        </p>
-        <p>
-          "Eighty percent of success is showing up."
-          <br />- Woody Allen
-        </p>
+        <div className="quotes">
+          <p className="singleQuote">
+            "The man who says he can, and the man who says he can not.. Are both
+            correct"
+          </p>
+          <p className="singleQuote quoteAuthor">- Confucius</p>
+          <p className="singleQuote">
+            "It is better to be a warrior in a garden, than a gardener in war."
+          </p>
+          <p className="singleQuote quoteAuthor">- Unknown</p>
+          <p className="singleQuote">
+            "Eighty percent of success is showing up."
+          </p>
+          <p className="singleQuote quoteAuthor">- Woody Allen</p>
+        </div>
       </div>
     </div>
   );
