f69da4982b48145d2e8c5faa6f5b5387e6bd438b
ajmcf52 <andrew.mcfarlane52@gmail.com>
2022-10-26 14:38:07 -0700

    progress made on ProjectsPage; switching back to working on the main branch.

--- src/Components/HomePageHeader.js
+++ src/Components/HomePageHeader.js
@@ -21,32 +21,33 @@ const HomePageHeader = () => {
         </p>
         <p className="blurb">
           <span className="topBlurbChunk">
-            My big interests are programming, exercise (i.e., weightlifting,
-            hiking, running, etc), non-fiction books, writing, international
-            relations, and mythology. With writing, I enjoy creating mostly
-            prose and poetry. When time and responsibility afford it, I enjoy
-            hanging with friends, watching films, and getting outside.
+            My interests are programming, exercise (i.e., weightlifting, hiking,
+            running, etc), non-fiction, and writing. With writing, I enjoy
+            creating mostly prose and poetry. When time and responsibility
+            afford it, I enjoy hanging with friends, watching films, and getting
+            outside.
           </span>{" "}
           <br /> <br />
           <span className="textChunk">
-            If you are interested in checking out some of my software projects,{" "}
-            <b>
-              feel free to give them a look <a href="#top">here</a>.
-            </b>
+            If you are interested in checking out some of my programming
+            projects, feel free to check them out{" "}
+            <a href="./ProjectsPage.js">here</a>.
           </span>
           <br />
           <br />
           <span className="textChunk">
             For the past two years, I have been kicking myself to start a blog.
-            I finally did it. You might find it interesting. Take a look{" "}
-            <a href="#top">here</a>.
+            I finally did it, and you may or may not find it interesting. Take a
+            look <a href="#top">here</a>.
           </span>
           <br />
           <br />
           <span className="textChunk">
-            I've kept note of the books I've read over the past couple years, as
-            well as books I'm currently reading and plan on reading. If you
-            care, you can see a list of these <a href="#top">here</a>.
+            I've found that much can be gleaned from a person based on what they
+            choose to read. I've kept note of the books I've read over the past
+            few years, as well as books I'm currently reading and plan on
+            reading. If you care, you can see a list of these{" "}
+            <a href="./ReadingListPage.js">here</a>.
           </span>
         </p>
         <div className="quotes">
