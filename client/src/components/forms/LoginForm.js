728d660e72775e7138c51fef0b96911cc162e26f
ajmcf52 <andrew.mcfarlane52@gmail.com>
2023-01-04 18:07:52 -0800

    LoginForm hooked into the redux store as a functional component.

--- client/src/components/forms/LoginForm.js
+++ client/src/components/forms/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState, useEffect, useContext } from "react";
+import { connect } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import {
     FormControlLabel,
@@ -8,6 +9,7 @@ import {
 } from "@mui/material";
 import { StyledField } from "../misc/StyledField";
 import AuthContext from "../../context/AuthProvider";
+import { LoginEventCreator } from "../../actions/LoginEvent";
 import axios from "../../api/axios";
 import "./css/LoginForm.css";
 
@@ -20,7 +22,7 @@ const errorMsgStyle = (shouldDisplayError) =>
         },
     });
 
-const LoginForm = () => {
+const LoginForm = ({ triggerLogin }) => {
     const navigate = useNavigate();
     const { setAuth } = useContext(AuthContext);
     const emailRef = useRef();
@@ -58,7 +60,7 @@ const LoginForm = () => {
                 .then((res) => {
                     console.log(JSON.stringify(res?.data));
                     const { accessToken, fname, email } = res.data;
-
+                    triggerLogin({ email, fname });
                     setAuth({ email, pword, accessToken });
                 });
             setEmail("");
@@ -162,134 +164,8 @@ const LoginForm = () => {
     );
 };
 
-export default LoginForm;
+const mapDispatchToProps = {
+    triggerLogin: LoginEventCreator.login,
+};
 
-// import React from "react";
-// import axios from "axios";
-// import { connect } from "react-redux";
-
-// import {
-//     FormControlLabel,
-//     Checkbox,
-//     Typography,
-//     createStyles,
-// } from "@mui/material";
-// import BackButton from "../buttons/BackButton";
-// import { StyledField } from "../misc/StyledField";
-// import { LoginEventCreator } from "../../actions/LoginEvent";
-// import { withRouter } from "../../utils/withRouter";
-// import "./css/LoginForm.css";
-
-// const errorMsgStyle = (shouldDisplayError) =>
-//     createStyles({
-//         root: {
-//             display: shouldDisplayError ? "table" : "none",
-//             minHeight: "30px",
-//             minWidth: "100px",
-//         },
-//     });
-
-// class LoginForm extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             email: "",
-//             password: "",
-//             showPassword: false,
-//             err: false,
-//             errtext: "",
-//         };
-//         this.navigateToLanding = this.navigateToLanding.bind(this);
-//     }
-
-//     handleChange = (evt) => {
-//         this.setState(() => ({
-//             [evt.target.name]: evt.target.value,
-//         }));
-//     };
-
-//     handleSubmit = (evt) => {
-//         evt.preventDefault();
-//         const { email, password } = this.state;
-//         axios
-//             .post("http://localhost:8000/login", { email, password })
-//             .then((res) => {
-//                 for (var header of res.headers) {
-//                     console.log("HEADER --> ", header);
-//                 }
-//                 this.setState({ err: false, errtext: "" });
-//                 this.props.triggerLogin({
-//                     email: email,
-//                     fname: res.data.fname,
-//                 });
-//                 this.navigateToLanding();
-//             })
-//             .catch((error) => {
-//                 console.log("Login Error! --> ", error);
-//                 this.setState({
-//                     err: true,
-//                     errtext: error.response.data.errText,
-//                 });
-//             });
-//     };
-
-//     navigateToLanding() {
-//         this.props.navigate("/");
-//     }
-
-//     render() {
-//         return (
-//             <div className="top-level">
-//                 <BackButton className="back-btn" />
-//                 <div className="form-container login-color">
-//                     <form
-//                         name="login"
-//                         method="post"
-//                         className="form-login shadow-layering"
-//                         action=""
-//                         encType="multipart/form-data"
-//                         onSubmit={this.handleSubmit}>
-//                         <h1 className="title">Login</h1>
-// <div className="input-container">
-//     <StyledField
-//         required
-//         id="email-field"
-//         className="field"
-//         fullWidth
-//         label="Email"
-//         defaultValue=""
-//         onChange={this.handleChange}
-//         variant="outlined"
-//         name="email"
-//     />
-// </div>
-//                         <div
-//                             className="error-msg"
-//                             style={errorMsgStyle(this.state.err).root}>
-//                             <Typography
-//                                 className="css-ahj2mt-MuiTypography-root"
-//                                 style={{
-//                                     fontSize: "small",
-//                                     color: "red",
-//                                     position: "relative",
-//                                     minHeight: "30px",
-//                                 }}>
-//                                 {this.state.errtext}
-//                             </Typography>
-//                         </div>
-// <input
-//     type="submit"
-//     className="submit-button"
-//     value="Submit"></input>
-//                     </form>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
-// const mapDispatchToProps = {
-//     triggerLogin: LoginEventCreator.login,
-// };
-
-// export default connect(null, mapDispatchToProps)(withRouter(LoginForm));
+export default connect(null, mapDispatchToProps)(LoginForm);
