a44a711db361fe9aa3018061a7631e433b8c175d
ajmcf52 <andrew.mcfarlane52@gmail.com>
2023-01-24 17:53:36 -0800

    mostly planning; getting ready to develop the ShopPage.

--- client/src/components/main/ShopPage.js
+++ client/src/components/main/ShopPage.js
@@ -20,12 +20,39 @@ const navShopBtnTheme = createTheme({
 });
 
 export default function ShopPage() {
-    let navigate = useNavigate();
+    /**
+    in order to display the shoes that we have for sale,
+    we will need to do the following:
+    - make an API call that retrieves an array of tuples,
+    where each tuple contains all the info required for a given shoe.
+    the display square for each shoe requires:
+    - the shoe name
+    - the price
+    - a picture
+
+    For each shoe, the API call will return the shoe name, price, and picture URL.
+    We will then put the URL into an <img> tag and go from there. This might need
+    tweaking, but this is the approach we will go with for now.
+
+    This is the basic premise of getting the shoes loaded in.
+
+    Then, each of the pictures themselves should be clickable links (as well as the shoe name below).
+
+    Clicking any given shoe link will take the user to a page detailing the specific info for that shoe,
+    giving the user the option to select a size, as well as add 1 or more to their cart.
+
+    */
+
     return (
         <div className="shop-root">
             <NavBar
                 theme={navShopBtnTheme}
                 buttons={[<LoginButton />, <SignupButton />]}></NavBar>
+
+            <header>
+                <h2 className="shop-header">Check these puppies out!</h2>
+            </header>
+            <div className="selection-container"></div>
         </div>
     );
 }
