cf5fd0ed3cca5a1ff08f0fdc59a040b8699ee584
ajmcf52 <andrew.mcfarlane52@gmail.com>
2023-03-09 16:21:48 -0800

    retrying interceptor for invalid access tokens; finished /cartContents

--- api/routes/cartContents.js
+++ api/routes/cartContents.js
@@ -1,14 +1,32 @@
 var express = require("express");
+var jwt = require("jsonwebtoken");
 var authConfig = require("../config/authConfig");
 var connection = require("../config/connection");
-var cartInitUtil = require("../routes/initCart");
+var cartInitUtil = require("./initCart");
 const confirmRefreshToken = require("../util/confirmRefreshToken");
 
 var router = express.Router();
 
 router.get("/validateCart", async (req, res) => {
     var email = req.cookies.shoeDawgUserEmail;
+    var accessToken = req.body.at || req.query.at;
     var refreshToken = req.cookies.shoeDawgRefreshToken;
+
+    if (accessToken === undefined) {
+        res.status(449).send({
+            errText: "Access Token is missing; please log in.",
+        });
+        return;
+    }
+    try {
+        jwt.verify(accessToken, authConfig.secret);
+    } catch (error) {
+        res.status(449).send({
+            errText: `Error while verifying access token: ${error}`,
+        });
+        return;
+    }
+
     if (refreshToken === undefined || email === undefined) {
         res.status(403).send({
             errText: "Email and/or RT is undefined; please log in.",
@@ -18,7 +36,7 @@ router.get("/validateCart", async (req, res) => {
     const [results] = await confirmRefreshToken(email, refreshToken);
     if (results.length === 0) {
         res.status(403).send({
-            errText: "No RT stored in database; please log in.",
+            errText: "Matching RT not found; please log in.",
         });
         return;
     }
@@ -49,13 +67,17 @@ router.get("/validateCart", async (req, res) => {
     // at this point, cart is active. Must get contents.
 
     const getCartContents = async () => {
-        var sql = `SELECT sku, quantity FROM CART_DETAILS NATURAL JOIN ON CART WHERE refresh_token=?`;
+        //var sql = `SELECT sku, quantity FROM CART_DETAILS NATURAL JOIN ON CART WHERE refresh_token=?`;
+        var sql = `SELECT CD.sku, CD.quantity, name, price FROM CART_DETAILS CD NATURAL JOIN CART JOIN STOCK S1 WHERE CD.sku=S1.sku AND refresh_token=?`;
         const [results] = await (await connection).execute(sql, [matchingToken]);
         let resultArray = [];
         for (var i = 0; i < results.length; i++) {
             let resObj = {};
             resObj.sku = results[i]["sku"];
             resObj.quantity = results[i]["quantity"];
+            resObj.name = results[i]["name"];
+            resObj.price = results[i]["price"];
+            resultArray.push(resObj);
         }
         return resultArray;
     };
